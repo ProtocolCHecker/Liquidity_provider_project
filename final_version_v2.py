a738fcc6bdf964597b2a5221f597a8e164b759f7
ProtocolChecker <florian.bargues@fountainhead.digital>
2025-01-20 02:12:36 -0300

    final

--- final_version_v2.py
+++ final_version_v2.py
@@ -64,13 +64,6 @@ def get_token_total_supply(chain, token_address, abi):
         return None
     return total_supply
 
-# Function to fetch and process data
-def fetch_and_process_data(query_id):
-    dune = DuneClient(api_key='bDYuuMloPmsCwKEERSOkZajlJCkQYt9Z')
-    results_response = dune.get_latest_result(query_id)
-    results = results_response.result.rows
-    total_supply = sum(row['current_balance'] for row in results) / 1000000
-    return results, total_supply
 
 # Function to plot Sunburst charts with tables
 def plot_sunburst_chart(holders_data, total_supply, asset, borrow_rate, lending_rate, utilization_rate, token_type):
@@ -177,15 +170,6 @@ def get_token_holder_chart(chain, contract_address, range_value=100):
         'Base': 'https://basescan.org/token/tokenholderchart/'
     }
 
-    # Define the cookies for different chains
-    cookies = {
-        'Ethereum': 'ASP.NET_SessionId=d212cjpgl5zazkufnnaezfzv; etherscan_offset_datetime=-3; etherscan_switch_token_amount_value=value; etherscan_hidepoortokenalert=True; etherscan_hidezeroalert=True; __stripe_mid=69c4398a-9735-4550-a705-f760fd3a309c7be9ad; __cflb=02DiuFnsSsHWYH8WqVXbZzkeTrZ6gtmGUrqFkD4VCumWt; cf_clearance=mzwrR48P2aRwOw1Dd3g1XdExQajASCtP.60my97Etro-1737263390-1.2.1.1-iWIpA.KnEApkB6388lPQ0W0K.ZW7F4gxUaB7OOTIjzMxxEs6tMF2Gl.q0xatieydswrPJYIFVfDBCWwq_WWeoUrNDzbG2KxX6Mur7Hey73UxYnqcztQu95cVoqiiD_RpR3SJ5WE5MJNZXalch7p.7WAIlZVOf.rf0.GIDUwzFS9tN14j7meq3Kg5SADToc4XvgJVdp0SWi8sCACmz7n0wjmViTbahYTsYJH_f2qpmh4y_QueCJipbONigUCIPNVfuEpbmI_0dWI6bW5JJtPZkhGQfw2XT9ngfSPXu_DR5lidVvpv5pv4jL4ESElGhvs5kWZUBdgCU6KxWFk_uQCzaw',
-        'Optimism': 'ASP.NET_SessionId=20o34auacn5snbgujxcdrjix; op mainnet etherscan_offset_datetime=-3; op mainnet etherscan_switch_token_amount_value=value; __stripe_mid=69c4398a-9735-4550-a705-f760fd3a309c7be9ad; __cflb=02DiuJ7NLBYKjsZtxjRR4QggQcq1CaL9Q7kUxbnvtjycQ; cf_clearance=bAWAdCKBS9Z1EfvbLTUqyCEbUC0p47GMN34rAJKBAOk-1737264993-1.2.1.1-mOuGv2YgUpgME.LMqhobcNuqrZx7AjDomHvpARWSRBYhcHVL7gtfOR1.vDmhn1d90aPfKhgOlDUGQkxhakaSt.17xRF6RsWSiFh92fI6eaMsj0LvZ76CtRnE1NvWRcEU2OP9k37biPQcEfKRjOAmKRzZ7jATxuqM_nt8oRkkn63tMOHbqVK7uuMGRq6nPlOZ17nryNFNXfH77umUqUdzqUzKM.R3j1VSF9zwMr58oQbeVA25TGMXH1mGuDmw1eGO_1tLiKakOE.06aYzjgVNVv.Ml2CVJ9g7DuAartYNshIRWdbeKO8SLMIeiLZvUc.Co6D6rBegI3k_pcT_jKnCTQ',
-        'Polygon': 'ASP.NET_SessionId=zen30zd42soml15bbdxdv4pt; polygonscan_offset_datetime=-3; polygonscan_switch_token_amount_value=value; __cflb=0H28vYYxgAifymwG4Xt78Bvs2KmKYds75LU77NckDX2',
-        'Arbitrum': 'ASP.NET_SessionId=z5tw5gppprxwuogbo0kehcs5; arbitrum one_offset_datetime=-3; arbitrum one_switch_token_amount_value=value; _ga=GA1.1.972759942.1736862088; _ga_KD34MKJY8D=GS1.1.1736862087.1.0.1736862087.0.0.0; cf_clearance=QrPf36WjIxcosACeUCUzJyhN77BS.ebyZGkbX8WxAwM-1737055489-1.2.1.1-kKH2vOYp88TkgUnN20our9UJ1xMm40i3R6vqTPSbahi9rmRddxTIw56SNo0DYgQolcjX3zaKGUnBvLL6XrVAYHXZa5sYENpEZwrD2M9vObvT6jfODu.QXKiV95HgInWfq49NFETccYN0_FtSeCY19NuDTEQKRJFwKWmeUuLhOvAtgBlbhOJgYWn2l3TOk.Id1Axh48W.arGD.G4MMHwbkF3Llvg2br5DHyqHex6YXP5ilp5yjSzVw93BOfs1.EU76P4tPCSgaOci_h7pgZ4tj8H8NAC7TT3.Vy4nKRE2CNUWaL5EJ7dQaD210Sv10cm.xpHO1Iio3rXO6.xpxqJawQ; __cflb=02DiuJLycbJL3fMraug7KdEi8GAxienVNSKUy1xT1iaJY',
-        'Base': 'ASP.NET_SessionId=syjjkhlthnrk2n2vc5nxdhsf; basescan_offset_datetime=-3; basescan_switch_token_amount_value=value; __cflb=02DiuJ1fCRi484mKRwML12UraygpucsySf7Q3gY684tVz; cf_clearance=f2H1ps2b7PSX9SylscTKyFhfrlIue..YFPYvrYUfO5E-1737264686-1.2.1.1-x0TlUDTsM5ixWGLGu8yVkF66xQ8MU.RNShyc_0eE6krZEVkAxksOEsEbh0zhS96VDEdfV1.6zp_o3T2Ft22Wzh14LaKyA9qFkulL51AuE9rHSTnJ9cE0YZufa6WCMFljhSvmgctaSZ31wc8hiqVxNP15c89KNcrERNsRWrvBQ9_mTvmvoMu.IUfnPvORJCPyCjG9zct.a3YxQySVoJpI1Lz7aRAFHjbQFmIJsTvGiMGCs7u_XkMyzFexM1TrBa4ivsGuQcSHQRSgdfdlkBSQrIKnJcVOZBV81_ZWnHhOg.U7HLB02PS1CpMEaleJUJ79OUby0HmskjPKVpmPdU8OxA'
-    }
-
     # Validate the chain parameter
     if chain not in base_urls:
         raise ValueError(f"Unsupported chain: {chain}")
@@ -198,7 +182,6 @@ def get_token_holder_chart(chain, contract_address, range_value=100):
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'accept-language': 'en-GB,en;q=0.9',
         'cache-control': 'max-age=0',
-        'cookie': cookies[chain],
         'priority': 'u=0, i',
         'referer': url,
         'sec-ch-ua': '"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
@@ -408,9 +391,6 @@ if user_input:
                         </div>
                     </div>
                 """, unsafe_allow_html=True)
-
-                a_query_id = asset['dune_a_id']
-                debt_query_id = asset['dune_debt_id']
 
                 a_results, a_total_supply = get_token_holder_chart(chain, a_token_address), a_token_supply
                 debt_results, debt_total_supply = get_token_holder_chart(chain, debt_token_address), debt_token_supply
